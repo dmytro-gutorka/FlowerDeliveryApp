38d4a37ab2388e6c92ceb1ab3fc96bb4a773edcb
Dmytro Gutorka <dgutorka@gmail.com>
2025-09-11 23:59:31 +0300

    Add Header component

--- frontend/src/app/theme.ts
+++ frontend/src/app/theme.ts
@@ -3,8 +3,12 @@ import {createTheme} from "@mui/material";
 export const theme = createTheme({
     palette: {
         mode: 'light',
-        // primary: { main: '#7c3aed' },
-        // secondary: { main: '#10b981' },
+        // primary: { main: 'rgb(219, 39, 119)' },
+        secondary: { main: '#10b981' },
     },
     shape: { borderRadius: 12 },
 });
+
+
+
+// rgb(219, 39, 119)
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,72 +1,68 @@
+import BookmarkAddOutlinedIcon from '@mui/icons-material/BookmarkAddOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Badge from '@mui/material/Badge';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import BookmarkAddOutlinedIcon from '@mui/icons-material/BookmarkAddOutlined';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import React, {useState} from "react";
+import {Container, Icon, Stack, SvgIcon} from "@mui/material";
 
-
-export default function PrimarySearchAppBar() {
+export default function Header() {
     const cartItems = 1
     const favoriteItems = 1
 
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState<null | HTMLElement>(null);
-
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
+
+        <AppBar position="static">
+        <Container maxWidth="xl">
+                <Toolbar >
+
+                    <Stack direction="row" gap={1} alignItems="center">
+                    <SvgIcon fontSize="large">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"
+                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                             stroke-linejoin="round" className="lucide lucide-flower2 h-8 w-8">
+                            <path
+                                d="M12 5a3 3 0 1 1 3 3m-3-3a3 3 0 1 0-3 3m3-3v1M9 8a3 3 0 1 0 3 3M9 8h1m5 0a3 3 0 1 1-3 3m3-3h-1m-2 3v-1"></path>
+                            <circle cx="12" cy="8" r="2"></circle>
+                            <path d="M12 10v12"></path>
+                            <path d="M12 22c4.2 0 7-1.667 7-5-4.2 0-7 1.667-7 5Z"></path>
+                            <path d="M12 22c-4.2 0-7-1.667-7-5 4.2 0 7 1.667 7 5Z"></path>
+                        </svg>
+                    </SvgIcon>
                     <Typography
-                        variant="h6"
+                        variant="h5"
                         component="div"
+                        fontWeight={900}
                         noWrap
                         sx={{ display: { xs: 'none', sm: 'block' } }}
                     >
                         FlowerShop
                     </Typography>
-                    <Box sx={{ flexGrow: 1 }} />
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label={`${favoriteItems} items in your favorite list`}
-                            color="inherit"
-                        >
-                            <Badge badgeContent={cartItems} color="error">
-                                <BookmarkAddOutlinedIcon />
+
+                    </Stack>
+
+
+                    <Box>
+                        <IconButton size="large" aria-label="Open favorites">
+                            <Badge badgeContent={favoriteItems} color="error">
+                                <BookmarkAddOutlinedIcon/>
                             </Badge>
                         </IconButton>
                     </Box>
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label={`${cartItems} items in your cart`}
-                            color="inherit"
-                        >
+                    <Box>
+                        <IconButton size="large" aria-label="Open cart" color="inherit">
                             <Badge badgeContent={cartItems} color="error">
                                 <ShoppingCartOutlinedIcon />
                             </Badge>
                         </IconButton>
                     </Box>
-                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="show more"
-                            aria-controls="mobile-menu-drawer"
-                            aria-haspopup="true"
-                            onClick={(e: React.MouseEvent<HTMLButtonElement>) => setMobileMoreAnchorEl(e.currentTarget)}
-                            color="inherit"
-                        >
-                            <MoreIcon />
-                        </IconButton>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            {/*<ContextMenu></ContextMenu>*/}
 
-        </Box>
+
+                </Toolbar>
+        </Container>
+        </AppBar>
+
     );
 }
